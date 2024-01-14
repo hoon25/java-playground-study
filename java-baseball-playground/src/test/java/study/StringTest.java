000629c5dc3f7e0ef29d0729e5be75a8604b992d
parkchoongho <pchyo92@gmail.com>
2024-01-14 22:44:08 +0900

    📖 단위테스트 학습 1단계 - 야구게임

--- java-baseball-playground/src/test/java/study/StringTest.java
+++ java-baseball-playground/src/test/java/study/StringTest.java
@@ -3,6 +3,10 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -42,4 +46,66 @@ public class StringTest {
             "(1,2)".charAt(5);
         }).isInstanceOf(StringIndexOutOfBoundsException.class).hasMessageContaining("String index out of range: 5");
     }
+
+    @Test
+    @DisplayName("String 클래스 사친 연산 계산기 테스트")
+    void calculate() {
+        String input = "2 + 3 * 4 / 2";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Scanner scanner = new Scanner(System.in);
+        String statement = scanner.nextLine();
+
+        String[] values = statement.split(" ");
+
+        Integer result = 0;
+        String calculation = "+";
+        for (Integer i = 0; i < values.length; i++){
+            if (i % 2 == 1) {
+                calculation = values[i];
+                continue;
+            }
+            if (i % 2 == 0) {
+                if (calculation.equals("+")) result += Integer.valueOf(values[i]);
+                if (calculation.equals("-")) result -= Integer.valueOf(values[i]);
+                if (calculation.equals("*")) result *= Integer.valueOf(values[i]);
+                if (calculation.equals("/")) result /= Integer.valueOf(values[i]);
+            }
+        }
+
+        assertThat(result).isEqualTo(10);
+        scanner.close();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
