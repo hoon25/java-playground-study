8688725037c55adcd06d9f205871bf691e6731da
parkchoongho <pchyo92@gmail.com>
2024-01-22 06:29:55 +0900

    ♻️  make result local variable to instance variable

--- java-baseball-playground/src/main/java/Calculator.java
+++ java-baseball-playground/src/main/java/Calculator.java
@@ -1,14 +1,15 @@
 public class Calculator {
-    public CalculationSymbol symbol = CalculationSymbol.PLUS;
+    private CalculationSymbol symbol = CalculationSymbol.PLUS;
+    private int result = 0;
+
     public int getResult(String statement) {
         String[] values = statement.split(" ");
 
-        int result = 0;
-
-        for (String value: values) {
-            result = this.calculate(result, value);
+        for (String value : values) {
+            this.calculate(value);
             this.setSymbol(value);
         }
+
         return result;
     }
 
@@ -16,19 +17,18 @@ public class Calculator {
         return str.chars().allMatch(Character::isDigit);
     }
 
-    private int calculate(int previous, String value){
-        if (!this.isNumeric(value)) return previous;
+    private void calculate(String value) {
+        if (!this.isNumeric(value)) return;
 
         int addition = Integer.parseInt(value);
-        if (symbol == CalculationSymbol.PLUS) previous += addition;
-        if (symbol == CalculationSymbol.MINUS) previous -= addition;
-        if (symbol == CalculationSymbol.MULTIPLY) previous *= addition;
-        if (symbol == CalculationSymbol.DIVIDE) previous /= addition;
-        return previous;
+        if (symbol == CalculationSymbol.PLUS) result += addition;
+        if (symbol == CalculationSymbol.MINUS) result -= addition;
+        if (symbol == CalculationSymbol.MULTIPLY) result *= addition;
+        if (symbol == CalculationSymbol.DIVIDE) result /= addition;
     }
 
     private void setSymbol(String value) {
-        if(!CalculationSymbol.contains(value)) return;
+        if (!CalculationSymbol.contains(value)) return;
 
         if (value.equals("+")) symbol = CalculationSymbol.PLUS;
         if (value.equals("-")) symbol = CalculationSymbol.MINUS;
